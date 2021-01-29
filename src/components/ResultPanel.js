c7ad198ab18a6c008ee0c0ff708ba3eb9b1628a2
alexandre <alexandre.vanhoutte@gmail.com>
2021-01-29 15:59:55 +0900

    feat(logic): filter emoji with keyword + design

--- src/components/ResultPanel.js
+++ src/components/ResultPanel.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import ResultCard from '../components/ResultCard'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import ResultCard from '../components/ResultCard';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  grid: {
+    maxWidth: 800
+   }
+});
 
 function ResultPanel({emojiListProps}) {
+
+  const classes = useStyles();
 
   function displayResult() {
     if (emojiListProps != null) {
       var resultCardList = []
-      emojiListProps.map((item, index) => resultCardList.push(<ResultCard key={index} item={item} />))
+      emojiListProps.map((item, index) => resultCardList.push(
+        <Grid item xs={6} spacing={3}>
+          <ResultCard key={index} item={item} />
+        </Grid>))
       return resultCardList
     } else {
       return ("Nothing to display")
@@ -14,8 +31,10 @@ function ResultPanel({emojiListProps}) {
   }
 
   return (
-    <div>
-      {displayResult()}
+    <div className={classes.root}>
+      <Grid container className={classes.grid} direction="row" justify="center" alignItems="center" spacing={2}>
+        {displayResult()}
+      </Grid>
     </div>
   )
 
--- src/logic/FilterEmoji.js
+++ src/logic/FilterEmoji.js
@@ -2,10 +2,11 @@ import emoji_list from './emoji'
 
 
 function FilterEmoji(search) {
-  console.log(search)
-  var result = emoji_list.filter(({name}) => name.includes(search)).slice(0, 20)
-  console.log(result)
-  return result
+  if (search != null && search) {
+    return emoji_list.filter(({name}) => name.includes(search)).slice(0, 20)
+  } else {
+    return emoji_list.slice(0, 20)
+  }
 }
 
 export default FilterEmoji;
