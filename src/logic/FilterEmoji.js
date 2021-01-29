04a582611ebddcdeba3fd0d4042f11978e0eb272
alexandre <alexandre.vanhoutte@gmail.com>
2021-01-29 16:04:24 +0900

    fix(filter): add lowercase function

--- src/logic/FilterEmoji.js
+++ src/logic/FilterEmoji.js
@@ -3,7 +3,7 @@ import emoji_list from './emoji'
 
 function FilterEmoji(search) {
   if (search != null && search) {
-    return emoji_list.filter(({name}) => name.includes(search)).slice(0, 20)
+    return emoji_list.filter(({name}) => name.includes(search.toLowerCase())).slice(0, 20)
   } else {
     return emoji_list.slice(0, 20)
   }
